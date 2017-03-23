f1c6dbe9434bf335f78caf4db6968bcb87a73a19
Carlo Supina <cdsupina@gmail.com>
2017-03-23 12:00:55 -0400

    change images paths in constants file to images to improve effeciency of map area loading

--- game.love/tile.lua
+++ game.love/tile.lua
@@ -1,4 +1,5 @@
 require 'AnAL'
+require 'constants'
 
 Tile = {}
 Tile.__index = Tile
@@ -11,8 +12,8 @@ setmetatable(Tile, {
 	end,
 })
 
-function Tile:_init(name ,image_path, traverseable)
-	self.image = love.graphics.newImage(image_path)
+function Tile:_init(name ,image, traverseable)
+	self.image = image
 	self.traverseable = traverseable
 	self.tile_type = 'static'
 	self.animated = false
@@ -31,8 +32,8 @@ setmetatable(AnimatedTile, {
 	end,
 })
 
-function AnimatedTile:_init(name, image_path, traverseable, animation_delay)
-	Tile._init(self, name, image_path, traverseable)
+function AnimatedTile:_init(name, image, traverseable, animation_delay)
+	Tile._init(self, name, image, traverseable)
 	self.animation_delay = animation_delay
 	self.animation = newAnimation(self.image,16,16,self.animation_delay,0)
 	self.tile_type = 'animated'
@@ -52,8 +53,8 @@ setmetatable(TeleTile, {
 	end,
 })
 
-function TeleTile:_init(name, image_path, traverseable, target_index, target_x, target_y)
-	Tile._init(self, name, image_path, traverseable)
+function TeleTile:_init(name, image, traverseable, target_index, target_x, target_y)
+	Tile._init(self, name, image, traverseable)
 	self.tile_type = 'tele'
 	self.animated = false
 	self.target_index = target_index
@@ -63,6 +64,26 @@ end
 
 function TeleTile:teleport(map, player)
 	map:setCurrentArea(self.target_index)
-	player.x = self.target_x*16
-	player.y = self.target_y*16
+	player.x = self.target_x*tile_size
+	player.y = self.target_y*tile_size
+end
+
+AnimatedTeleTile = {}
+AnimatedTeleTile.__index = AnimatedTeleTile
+
+setmetatable(AnimatedTeleTile, {
+	__index = AnimatedTile,
+	__call = function(cls, ...)
+		local self = setmetatable({}, cls)
+		self._init(...)
+		return self
+	end,
+})
+
+function AnimatedTeleTile:_init(name, image, traverseable, animation_delay, target_index, target_x, target_y)
+	AnimatedTile._init(self, name, image, traverseable, animation_delay)
+	self.tile_type = 'tele'
+	self.target_index = target_index
+	self.target_x = target_x
+	self.target_y = target_y
 end
